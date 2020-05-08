dcfbf9b292859f71357aea539da50df4f64d18bd
ctsProject-star <Shivangi.Sharma3@cognizant.com>
2020-05-09 01:31:08 +0530

    ExceptionHandeling
    Using try-catch block.

---
+++ TheaterService/src/main/java/com/cts/project/theater/controller/EmptyJsonResponse.java
@@ -0,0 +1,9 @@
+package com.cts.project.theater.controller;
+
+import com.cts.project.theater.entity.Theater;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+public class EmptyJsonResponse extends Theater {
+
+}
