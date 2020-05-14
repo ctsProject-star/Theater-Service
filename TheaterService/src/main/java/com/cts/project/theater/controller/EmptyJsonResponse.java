87a2f1dce332b49b0b97d10ec223f5ca22043b71
Shivangi Sharma <64692898+ctsProject-star@users.noreply.github.com>
2020-05-15 00:14:50 +0530

    Delete EmptyJsonResponse.java

--- TheaterService/src/main/java/com/cts/project/theater/controller/EmptyJsonResponse.java
+++
@@ -1,9 +0,0 @@
-package com.cts.project.theater.controller;
-
-import com.cts.project.theater.entity.Theater;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-@JsonSerialize
-public class EmptyJsonResponse extends Theater {
-
-}
